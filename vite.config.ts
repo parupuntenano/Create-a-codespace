90b39f532d8de826a20c29362240b7db47660b35
parupuntenano <haruharu170523@gmail.com>
2026-04-10 01:28:47 +0000

    new text

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // これがあるか確認
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './', // ← この1行を追加（ドットスラッシュ）
   plugins: [
     react(),
-    tailwindcss(), // これがあるか確認
+    tailwindcss(),
   ],
 })
